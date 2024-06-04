7a059953684505c56b9a45a6f00219cd6935e60d
velvet-jedi <pranjalruhela2811@gmail.com>
2024-06-04 15:19:36 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className='bg-gradient-to-r from-red-800 to-indigo-600 text-7xl text-center text-blue-50'>Hello</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
