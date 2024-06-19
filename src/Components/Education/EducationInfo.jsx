c5480b0917d4fb9594aeaf6b79fe27b819dc7e91
velvet-jedi <pranjalruhela2811@gmail.com>
2024-06-19 20:19:32 +0530

    present date calc

--- src/Components/Education/EducationInfo.jsx
+++ src/Components/Education/EducationInfo.jsx
@@ -3,14 +3,31 @@
 import '../../styles/EducationInfo.css';
 
 const EducationInfo = ({info}) => {
-    const {schoolName, degree, startDate, endDate} = info;
+    var {schoolName, degree, startDate, endDate} = info;
+    const today = new Date();
+
+    // parse the date string into a Date object
+    const parseDate = (dateString) => {
+        if(!dateString) return null;
+        const [year, month, day] = dateString.split('-');
+        return new Date(year, month-1, day);
+    }
+    
+    const parsedEndDate = parseDate(endDate);
+
+    // if the end date is in the future, set the end date to today
+    if(parsedEndDate && parsedEndDate > today) {
+        endDate = 'Present';
+    }
+
   return (
     <div className="education-info">
         <div className="education-info-group">
             <p className="dates">
                 {startDate}
-                {startDate && endDate && ' - '}
+                {startDate && endDate && ' to '}
                 {endDate}
+                
             </p>
         </div>
         <div className="education-info-group">
